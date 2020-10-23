8bda8c4c0845d2b44d99e94431b53544b6891e83
Emrudia <elisabarre@orange.fr>
2020-10-23 11:50:41 +0200

    fonctionne en local

--- sources/www/app.js
+++ sources/www/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
 const hostname = 'localhost'
-const port = 3000
+const port = 3002
 
-//Importing routes
-import routes from './routes/helloworld'
+const router=express.Router()
 
-//middleware
 app.use(express.static('public'));
 
-//routes
-app.use('/simplewebapp',routes);
+//middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
 
+//set route
+router.get('/', function(req, res) {
+  res.send('hello world');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://${hostname}:${port}`)
--- sources/www/routes/helloworld.js
+++ sources/www/routes/helloworld.js
@@ -1,7 +1,1 @@
-import { Router } from 'express';
 
-const router = Router();
-
-router.get('/', function(req, res) {
-  res.send('hello world');
-});
