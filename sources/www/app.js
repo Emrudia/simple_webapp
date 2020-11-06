849aa6bdede373690463ec97a07e54bded67ec69
Emrudia <elisabarre@orange.fr>
2020-11-06 10:42:40 +0100

    ajustement port et routage

--- sources/www/app.js
+++ sources/www/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/',wikiRouter);
+app.use('/express_routage/',wikiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
