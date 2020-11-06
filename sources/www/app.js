059cd08b4202ff058e8bf085e9843f194e9f72e0
Emrudia <elisabarre@orange.fr>
2020-11-06 09:37:07 +0100

    App simple avec GET/

--- sources/www/app.js
+++ sources/www/app.js
@@ -3,18 +3,12 @@ const app = express()
 const hostname = 'localhost'
 const port = 3002
 
-const router=express.Router()
 
-app.use('/simple_webapp/',express.static('public'));
+app.use(express.static('public'));
 
-//middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
 
 //set route
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
   res.send('hello world');
 });
 
